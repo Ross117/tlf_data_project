f8f83e84766c35dfe5f39ab19831fcf29395b9d7
Ross <rossmorran@hotmail.co.uk>
2025-03-10 20:04:14 +0000

    add fct_disruption model

---
+++ tfl_data/models/fct/fct_disruption.sql
@@ -0,0 +1,18 @@
+WITH src_disruption AS (
+    {# there may be duplicates in the source data #}
+    SELECT DISTINCT *
+    FROM {{ ref('src_disruption') }}
+),
+dim_line AS (
+    SELECT *
+    FROM {{ ref('dim_line') }}
+)
+
+SELECT
+    {{ dbt_utils.generate_surrogate_key(['d.line_name', 'd.description', 'd.time_received']) }} AS disruption_id,
+    l.line_id,
+    d.description,
+    d.closuretext AS delay_type,
+    d.time_received
+FROM src_disruption d
+LEFT JOIN dim_line l USING (line_name)
