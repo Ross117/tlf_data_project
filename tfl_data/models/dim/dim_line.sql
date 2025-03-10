5f75da9b8f80728f57b568af0f1d6ae4839e667e
Ross <rossmorran@hotmail.co.uk>
2025-03-10 20:03:59 +0000

    tweak field names

--- tfl_data/models/dim/dim_line.sql
+++ tfl_data/models/dim/dim_line.sql
@@ -4,7 +4,7 @@ WITH src_disruption AS (
 )
 
 SELECT
-    {{ dbt_utils.generate_surrogate_key(['line']) }} AS line_id,
-    line AS line_name
+    {{ dbt_utils.generate_surrogate_key(['line_name']) }} AS line_id,
+    line_name
 FROM src_disruption
 GROUP BY 1, 2
--- tfl_data/models/src/src_disruption.sql
+++ tfl_data/models/src/src_disruption.sql
@@ -6,7 +6,7 @@ WITH raw_disruption AS (
 )
 
 SELECT 
-    split_part(response:description, ':', 0) AS line,
+    split_part(response:description, ':', 0) AS line_name,
     split_part(response:description, ':', 2) AS description,
     response:closureText AS closureText,
     response:type AS type,
